3842517bf7708e042f1e57f13f9d8d3cd9f52722
maria eloize <152327260+mariaeloize@users.noreply.github.com>
2026-06-23 10:19:19 -0300

    Add files via upload

---
+++ PessoaAnonima.php
@@ -0,0 +1,14 @@
+<?php
+    class PessoaAnonima {
+
+        protected $nome;
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function setNome($nome) {
+            $this->nome = $nome;
+        }  
+    }
+?>
